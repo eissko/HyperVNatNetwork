463547586af0fe6c770f2a7b04e099526b7c7030
eissko <peter.weiss@pl.abb.com>
2018-07-28 21:29:55 +0200

    added commands

---
+++ createNatNetwork.ps1
@@ -0,0 +1,15 @@
+$MySwitchName="MyNatSwitch"
+$NetNatName="MyNatNetwork"
+$Gw="10.1.1.1"
+$Net="10.1.1.0/24"
+
+New-VMSwitch -SwitchName $MySwitchName -SwitchType Internal -Verbose
+$ifIndex=(Get-NetIPInterface | where-object {$_.InterfaceAlias -like "*$MySwitchName*"}).ifIndex
+New-NetIPAddress -IPAddress $GW -PrefixLength 24 -InterfaceIndex $ifIndex -Verbose
+New-NetNat -Name $NetNatName -InternalIPInterfaceAddressPrefix $Net -Verbose
+
+##inside vm
+## $ifIndex=(Get-NetAdapter).ifIndex
+##New-NetIPAddress -IPAddress 10.1.1.100 -DefaultGateway 10.1.1.1 -PrefixLength 24 -InterfaceIndex $ifIndex 
+##get-NetFirewallRule | Where-Object {$_.DisplayName -like "File and Printer Sharing (Echo Request - ICMPv4-In)" | Enable-NetFirewallRule
+##Set-DnsClientServerAddress -ServerAddresses 10.1.1.1 -InterfaceIndex $ifIndex
